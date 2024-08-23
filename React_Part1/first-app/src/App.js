392e55ac14c32d87b2e380ee9c950e0bed04f76e
MDAFSARALI <generaluserknown@gmail.com>
2024-08-23 14:05:03 +0530

    Intro To Jsx

---
+++ React_Part1/first-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button id='Btn'>
+        <span>
+          Submit
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default App;
