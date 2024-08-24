82f7225687194b81cf070ae1a6f9260ec907c199
MDAFSARALI <generaluserknown@gmail.com>
2024-08-24 15:34:04 +0530

    Use State Concept

--- React_Part2/part2/src/Counter.jsx
+++ React_Part2/part2/src/Counter.jsx
@@ -1,19 +1,44 @@
-import { useState } from "react";
-function Counter(){
-    const [x,setX]=useState(0);
-    const [y,setY]=useState(0);
-    return(
-        <>
-            count X:{x}
-            <button onClick={()=>setX(x+1)}>Incr</button>
-            <button onClick={()=>setX(x-1)}>Decr</button>
-            <br />
-            count Y:{y}
-            <button onClick={()=>setY(y+1)}>Incr</button>
-            <button onClick={()=>setY(y-1)}>Decr</button>
-       
-        </>
-    );
+// import { useState } from "react";
+// function Counter(){
+//     // const [x,setX]=useState(0);
+
+
+//     const [isEditing, setIsEditing]=useState(false);
+//     return(
+//         <>
+//             {/* count X:{x} is {(x%2 ==0) ? "Even" : "Odd"}
+//             <button onClick={()=>setX(x+1)}>Incr</button>
+//             <button onClick={()=>setX(x-1)}>Decr</button> */}
+//                 {       
+
+//                      (isEditing) ? <input/> : <p>Some todo</p>
+//                 }
+
+          
+//           <br />
+//           <button onClick={()=>setIsEditing(!isEditing)}> Click </button> 
+//         </>
+//     );
+// }
+
+// export default Counter;
+
+
+
+// Passing Arrays In the useState
+import { useState } from 'react'
+import React from 'react'
+
+const Counter = () => {
+
+    const [todos, setTodos]=useState(['todo 1','todo 2'])
+  return (
+    <ul>
+      {todos.map((todos) => <li>{todos}</li>)}
+      {/* <button onClick={()=>{setTodos([1,2,3])}}>Click</button> */}
+      <button onClick={()=>{setTodos([...todos,'another one'])}}>Click</button>
+    </ul>
+  )
 }
 
-export default Counter;
+export default Counter
